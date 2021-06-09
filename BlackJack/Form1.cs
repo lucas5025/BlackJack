4072e4902d171a685678053a484a3e8d666d7544
Lucas <lucasdebarros07@gmail.com>
2021-06-09 19:29:05 -0300

    atualização 2.0

--- BlackJack/Form1.cs
+++ BlackJack/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BlackJack
@@ -26,53 +19,59 @@ namespace BlackJack
         public void resultado()
         {
 
-            /*
-
-                jogador 1   x   jogador 2   resultado
-                <= 21           <= 21       maior ponto ganha   REGRA 1
-                > 21            <= 21       jogador 2 ganha     REGRA 2
-                <= 21           > 21        jogador 1 ganha     REGRA 3
-                > 21            > 21        2 perderam          REGRA 4
-                jogador 2       jogador 1   EMPATE              REGRA 5
-
-             */
-
-
-            if (pontos_A > pontos_B && pontos_A <= 21)
-                lbl_Resultado.Text = "Jogador 1 GANHOU!";
+            if (pontos_A == 21)
+            {
+                lbl_Resultado.Text = "Jogador 1 Ganhou!";
+            }
+            else if (pontos_B == 21)
+            {
+                lbl_Resultado.Text = "Jogador 2 Ganhou!";
+            }
+            else if (pontos_A <= 21 && pontos_B <= 21)
+            {
+                if (pontos_A > pontos_B)
+                    lbl_Resultado.Text = "Jogador 1 Ganhou!";
+                else
+                    lbl_Resultado.Text = "Jogador 2 Ganhou!";
+            }
+            else if (pontos_A > 21 && pontos_B <= 21)
+            {
+                lbl_Resultado.Text = "Jogador 2 Ganhou!";
+            }
+            else if (pontos_B > 21 && pontos_A <= 21)
+            {
+                lbl_Resultado.Text = "Jogador 1 Ganhou!";
+            }
+            else if (pontos_A == pontos_B)
+            {
+                lbl_Resultado.Text = "Empate";
+            }
             else
-                if (pontos_A < pontos_B && pontos_B <= 21)
-                lbl_Resultado.Text = "Jogador 2 GANHOU!";
-            else
-                    if (pontos_A <= 21 && pontos_B <= 21)
-                lbl_Resultado.Text = "EMPATE";
-            else
-                lbl_Resultado.Text = "SEM VENCEDOR.";
-
+                lbl_Resultado.Text = "Sem Vencedor!";
         }
 
         public void Jogada(PictureBox A, int jogador)
         {
-            int x, total_pontos=0;
+            int x, total_pontos = 0;
             Random sorteio = new Random();
             x = sorteio.Next(1, 14);
 
 
             switch (x)
             {
-                case 1:     A.Image = Properties.Resources.a;    total_pontos += 1;     break;
-                case 2:     A.Image = Properties.Resources._2;   total_pontos += 2;     break;
-                case 3:     A.Image = Properties.Resources._3;   total_pontos += 3;     break;
-                case 4:     A.Image = Properties.Resources._4;   total_pontos += 4;     break;
-                case 5:     A.Image = Properties.Resources._5;   total_pontos += 5;     break;
-                case 6:     A.Image = Properties.Resources._6;   total_pontos += 6;     break;
-                case 7:     A.Image = Properties.Resources._7;   total_pontos += 7;     break;
-                case 8:     A.Image = Properties.Resources._8;   total_pontos += 8;     break;
-                case 9:     A.Image = Properties.Resources._9;   total_pontos += 9;     break;
-                case 10:    A.Image = Properties.Resources._10;  total_pontos += 10;    break;
-                case 11:    A.Image = Properties.Resources.J;    total_pontos += 11;    break;
-                case 12:    A.Image = Properties.Resources.Q;    total_pontos += 12;    break;
-                case 13:    A.Image = Properties.Resources.K;    total_pontos += 13;    break;
+                case 1: A.Image = Properties.Resources.a; total_pontos += 1; break;
+                case 2: A.Image = Properties.Resources._2; total_pontos += 2; break;
+                case 3: A.Image = Properties.Resources._3; total_pontos += 3; break;
+                case 4: A.Image = Properties.Resources._4; total_pontos += 4; break;
+                case 5: A.Image = Properties.Resources._5; total_pontos += 5; break;
+                case 6: A.Image = Properties.Resources._6; total_pontos += 6; break;
+                case 7: A.Image = Properties.Resources._7; total_pontos += 7; break;
+                case 8: A.Image = Properties.Resources._8; total_pontos += 8; break;
+                case 9: A.Image = Properties.Resources._9; total_pontos += 9; break;
+                case 10: A.Image = Properties.Resources._10; total_pontos += 10; break;
+                case 11: A.Image = Properties.Resources.J; total_pontos += 11; break;
+                case 12: A.Image = Properties.Resources.Q; total_pontos += 12; break;
+                case 13: A.Image = Properties.Resources.K; total_pontos += 13; break;
             }
 
             if (jogador == 1)
@@ -81,17 +80,17 @@ namespace BlackJack
                 pontos_B += total_pontos;
         }
 
-            private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            
+
 
             Jogada(pictureBox1, 1);
-           
 
-            if(pontos_A <= 21)
+
+            if (pontos_A <= 21)
             {   // JOGANDO
                 lbl_Pontos_A.Text = Convert.ToString(pontos_A);
-                if(pontos_A == 21)
+                if (pontos_A == 21)
                 {
                     //lbl_Resultado.Text = "GANHOU!!!";
                     btn_jogar_1.Enabled = false;
@@ -102,7 +101,7 @@ namespace BlackJack
             {   // PARTIDA PERDIDA
                 lbl_Pontos_A.Text = Convert.ToString(pontos_A);
                 //lbl_Resultado.Text = "PERDEU!!!! ";
-                
+
                 btn_jogar_1.Enabled = false;
                 btn_parar_1.Enabled = false;
 
